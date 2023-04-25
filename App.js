f37f9eed0f07ffc3686d9bdde5f791901725c325
Fprobaos <probaosfelipe@gmail.com>
2023-04-24 23:49:02 -0300

    Haciendo lista dinamica con modal de remover Clase 5

--- App.js
+++ App.js
@@ -1,22 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { Text, View, TextInput, Button, FlatList, TouchableOpacity, Modal } from 'react-native';
+import { styles } from './style'; 
+import { useState } from 'react';
 
 export default function App() {
+
+  const [text, setText] = useState('')
+  const [events, setEvents] = useState([])
+  const [modalVisible, setModalVisible] = useState(false)
+  const [selectedEvent, setSelectedEvent] = useState(null)
+
+  const onAddList = () => {
+    if(text.length > 0)
+    return(
+      setEvents( [ ...events, {id: Math.random().toString(), value: text}]),
+      setText('')
+
+    )
+  }  
+
+  const onHandlerEvent = (id) => {
+    setModalVisible(true);
+    const selectedEvent = events.find(event => event.id === id)
+    setSelectedEvent(selectedEvent)
+  }
+
+  const renderItem = ({item}) => ( 
+    // aca hay un return implicito por eso los (). El render Item retorna un componente
+    <TouchableOpacity style={styles.itemContainer} onPress={() => onHandlerEvent(item.id)}>
+        <Text style={styles.item}> {item.value} </Text>
+    </TouchableOpacity>
+  )
+
+  const onHandleCancelModal = () => {
+    setModalVisible(!modalVisible)
+    setSelectedEvent(null)
+  }
+
+  const onHandleDeleteEvent = (id) => {
+    setModalVisible(!modalVisible)
+    setEvents(events.filter(event => event.id !== id))
+
+  }
+
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder='Enter your email' />
-        <Button title='Add' />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Enter your email' 
+          style={styles.input} 
+          value={text} 
+          onChangeText={(text) => setText(text)}/>
+        <Button title='Add' color={'#6761A8'} onPress={onAddList}/>
+        <Text />
       </View>
+      <View style={styles.listContainer}>
+        <FlatList renderItem={renderItem} data={events} keyExtractor={(item) => item.id}/>          
+      </View>
+
+
+      <Modal visible={modalVisible} animationType='slide'>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}> Event Detail</Text>
+
+          <View style={styles.modalDetailContainer}>
+            <Text style={styles.modalDetailMessage}>queres borrar este item?</Text>
+            <Text style={styles.selectedEvent}>{selectedEvent?.value}</Text>
+          </View>
+
+          <View style={styles.modalButtonContainer}> 
+            <Button title='Cancel' color='#52528C' onPress={() => onHandleCancelModal()} />
+            <Button title='Delete' color='#FF0011' onPress={() => onHandleDeleteEvent(selectedEvent.id)} />
+          </View>
+
+        </View>
+      </Modal>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexWrap: 'wrap',
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-
-}});
---
+++ style.js
@@ -0,0 +1,80 @@
+import { StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginHorizontal: 20,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    marginTop: 55,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  input: {
+    width: '80%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#52528C',
+    color: '#212121',
+  },
+  listContainer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      marginVertical: 20,
+  },
+  itemContainer: {
+      height: 80,
+      justifyContent: 'center',
+      backgroundColor: '#52528C',
+      borderRadius: 5,
+      marginVertical: 5,
+  },
+
+  item: {
+      marginHorizontal: 10,
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: 'bold',
+  },
+
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 60,
+    paddingVertical: 20
+  },
+
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+
+  modalDetailContainer: {
+    paddingVertical: 20,
+
+  },
+  modalDetailMessage:{
+    fontSize: 14,
+    color: ''
+
+  },
+
+  selectedEvent:{
+    fontSize: 14,
+    color: '#FF0011',
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+
+  modalButtonContainer:{
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+
+  }
+  });
+  
